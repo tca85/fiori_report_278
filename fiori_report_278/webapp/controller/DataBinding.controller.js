0f0e84abe5a05da4b304c8b992f253978c333f72
Thiago Cordeiro Alves <thiago.cordeiro.alves@sap.com>
2023-04-05 18:20:29 +0000

    Add Json model files and displaying the data

--- fiori_report_278/webapp/controller/DataBinding.controller.js
+++ fiori_report_278/webapp/controller/DataBinding.controller.js
@@ -7,8 +7,14 @@ sap.ui.define(
   
       return BaseController.extend("br.com.cliente.gestao.fiorireport278.controller.DataBinding", {
         onInit() {
+          var oModelJSON = new sap.ui.model.json.JSONModel;
+          oModelJSON.loadData("data/Produtos.json");
+
+          this.getView().setModel(oModelJSON, "Model_JSON_Produtos");
         }
+
       });
+
     }
   );
   
