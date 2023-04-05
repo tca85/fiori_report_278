5563bfedcda75ba7e58a18ae0f8a3c2b0121d126
Thiago Cordeiro Alves <thiago.cordeiro.alves@sap.com>
2023-04-05 19:25:26 +0000

    Databind one element in a specific control

--- fiori_report_278/webapp/controller/DataBinding.controller.js
+++ fiori_report_278/webapp/controller/DataBinding.controller.js
@@ -9,8 +9,20 @@ sap.ui.define(
         onInit() {
           var oModelJSON = new sap.ui.model.json.JSONModel;
           oModelJSON.loadData("data/Produtos.json");
-
           this.getView().setModel(oModelJSON, "Model_JSON_Produtos");
+        },
+
+        /**
+         * Simple form doesn't accept a collection only one item
+         * sap.ui.layout.form:SimpleForm
+         */
+        getRegion: function(){
+          var oRegionModelJSON = new sap.ui.model.json.JSONModel;
+          oRegionModelJSON.loadData("data/Regions.json");
+          this.getView().setModel(oRegionModelJSON, "Model_JSON_Regions");
+
+          var oForm = this.getView().byId("formRegions");
+          oForm.bindElement("Model_JSON_Regions>/regions/1");
         }
 
       });
