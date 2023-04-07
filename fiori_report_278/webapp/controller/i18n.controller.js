fbb15192f3fc0ce8a98a614d572a28ce77c2784c
Thiago Cordeiro Alves <thiago.cordeiro.alves@sap.com>
2023-04-07 09:47:40 +0000

    Create a new view and add a route for it

---
+++ fiori_report_278/webapp/controller/i18n.controller.js
@@ -0,0 +1,20 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("br.com.cliente.gestao.fiorireport278.controller.i18n", {
+        onInit() {
+
+
+        },
+
+      });
+
+
+
+    }
+  );
+  
