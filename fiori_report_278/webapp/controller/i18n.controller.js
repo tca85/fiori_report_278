65964a0a73d8a2f22e696fd044e897f7f8690962
Thiago Cordeiro Alves <thiago.cordeiro.alves@sap.com>
2023-04-07 10:38:47 +0000

    Read the form data and display in a MessageBox

--- fiori_report_278/webapp/controller/i18n.controller.js
+++ fiori_report_278/webapp/controller/i18n.controller.js
@@ -1,20 +1,30 @@
 sap.ui.define(
-    [
-        "sap/ui/core/mvc/Controller"
-    ],
-    function(BaseController) {
-      "use strict";
-  
-      return BaseController.extend("br.com.cliente.gestao.fiorireport278.controller.i18n", {
-        onInit() {
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageBox"
+  ],
+  function (BaseController, MessageBox) {
+    "use strict";
 
+    return BaseController.extend("br.com.cliente.gestao.fiorireport278.controller.i18n", {
+      onInit() {
+      },
 
-        },
+      /**
+       * Display the form data into a sap.m.messagebox
+       */
+      sendForm: function () {
+        //msgConfirmation
+        var oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
 
-      });
+        var customer = this.getView().byId("inpCustomer").getValue();
+        var city = this.getView().byId("inpCity").getValue();
+        var state = this.getView().byId("inpState").getValue();
 
+        var sMessage = oResourceBundle.getText("msgConfirmation", [customer, city, state]);
+        MessageBox.confirm(sMessage);
+      }
 
-
-    }
-  );
-  
+    });
+  }
+);
